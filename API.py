756441cc091a2be11db02a0c9f20a94000b72f29
snowyyfare8 <snowytejada08@gmail.com>
2025-11-16 14:37:36 +0300

    API mostly complete, geoloc rework.
    API still needs geolocator rework

--- API.py
+++ API.py
@@ -1,9 +1,14 @@
-# API Prototype
+from Application import city
 import openmeteo_requests
-
 import pandas as pd
 import requests_cache
 from retry_requests import retry
+import requests
+
+# Geolocator
+geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+geo_params = {"name": city, "count": 1}
+geo_res = requests.get(geo_url, params=geo_params).json()
 
 # API 1
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
@@ -11,41 +16,57 @@ retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 # API 2
+if "results" in geo_res:
+    lat = geo_res["results"][0]["latitude"]
+    lon = geo_res["results"][0]["longitude"]
 
-weather_url = "https://api.open-meteo.com/v1/forecast"
-weather_params = {
-    "latitude": 52.52,    
-    "longitude": 13.41,
-    "hourly": ["temperature_2m","relative_humidity_2m","wind_speed_180m","wind_direction_180m"],
-}
-responses = openmeteo.weather_api(weather_url, params=weather_params)
-    
-response = responses[0]
-print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation: {response.Elevation()} m asl")
-print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": ["temperature_2m", "relative_humidity_2m", "wind_speed_180m", "wind_direction_180m"],
+        "daily": ["uv_index_max"]
+    }
+    responses = openmeteo.weather_api(url, params=params)
 
-# Hourly Variables
-hourly = responses.Hourly()
-hourly_temperature_2m = hourly.variables(0).ValuesAsNumpy()
-hourly_relative_humidity_2m = hourly.variables(1).ValuesAsNumpy()
-hourly_wind_speed_180m = hourly.variables(2).ValuesAsNumpy()
-hourly_wind_direction_180m = hourly.variables(3).ValuesAsNumpy()
-hourly_uv_index = hourly.variables(4).ValuesAsNumpy()
+    response = responses[0]
+    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation: {response.Elevation()} m asl")
+    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-hourly_data = {"date": pd.date_range(
+    response = responses[0]
+    hourly = response.Hourly()
+    hourly_temp = hourly.Variables(0).ValuesAsNumpy()
+    hourly_humidity = hourly.Variables(1).ValuesAsNumpy()
+    hourly_windspeed = hourly.Variables(2).ValuesAsNumpy()
+    hourly_winddirection = hourly.Variables(3).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
         start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-        end = pd.to_datetime(hourly.Timeend(), unit = "s", utc = True),
+        end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
         freq = pd.Timedelta(seconds = hourly.Interval()),
         inclusive = "left"
-)}
+    )}
 
-# Assign hourly data to hourly variables
-hourly_data["temperature_2m"] = hourly_temperature_2m
-hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
-hourly_data["wind_speed_180m"] = hourly_wind_speed_180m
-hourly_data["wind_direction_180m"] = hourly_wind_direction_180m
-hourly_data["uv_index"] = hourly_uv_index
+    hourly_data["temperature_2m"] = hourly_temp
+    hourly_data["relative_humidity_2m"] = hourly_humidity
+    hourly_data["wind_speed_180m"] = hourly_windspeed
+    hourly_data["wind_direction_180m"] = hourly_winddirection
 
-hourly_dataframe = pd.DataFrame(data = hourly_data)
-print("\nHourly data\n", hourly_dataframe)
+    daily = response.Daily()
+    daily_uvindex = daily.Variables(0).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+
+    daily_data["uv_index_max"] = daily_uvindex
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    print("\nHourly data\n", hourly_dataframe)
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    print("\nDaily data\n", daily_dataframe)
+
