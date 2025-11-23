e7ed4ea8568b127fa7d27ea762e2f4b8adfe3138
snowyyfare8 <snowytejada08@gmail.com>
2025-11-23 17:38:51 +0300

    Enhance API to include hourly UV index data
    Added UV index to hourly data retrieval.

--- API.py
+++ API.py
@@ -46,6 +46,7 @@ if "results" in geo_res:
     hourly_humidity = hourly.Variables(1).ValuesAsNumpy()
     hourly_windspeed = hourly.Variables(2).ValuesAsNumpy()
     hourly_winddirection = hourly.Variables(3).ValuesAsNumpy()
+    hourly_uvindex = hourly.Variables(4).ValuesAsNumpy()
 
     hourly_data = {"date": pd.date_range(
         start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
@@ -58,20 +59,7 @@ if "results" in geo_res:
     hourly_data["relative_humidity_2m"] = hourly_humidity
     hourly_data["wind_speed_180m"] = hourly_windspeed
     hourly_data["wind_direction_180m"] = hourly_winddirection
-
-    daily = response.Daily()
-    daily_uvindex = daily.Variables(0).ValuesAsNumpy()
-
-    daily_data = {"date": pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-        freq = pd.Timedelta(seconds = daily.Interval()),
-        inclusive = "left"
-    )}
-
-    daily_data["uv_index_max"] = daily_uvindex
+    hourly_data["uv_index"] = hourly_uvindex
 
     hourly_dataframe = pd.DataFrame(data = hourly_data)
     print("\nHourly data\n", hourly_dataframe)
-    daily_dataframe = pd.DataFrame(data = daily_data)
-    print("\nDaily data\n", daily_dataframe)
