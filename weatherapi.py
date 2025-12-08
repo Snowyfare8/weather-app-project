3c73e61572a7f9d07ea2636e489d6a3112279470
snowyyfare8 <snowytejada08@gmail.com>
2025-12-08 13:21:11 +0300

    Add files via upload

---
+++ weatherapi.py
@@ -0,0 +1,106 @@
+# Weather API
+import openmeteo_requests
+import geocoder
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+import requests
+
+# Geolocator
+ip = geocoder.ip('me')
+city = ip.city 
+geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+geo_params = {"name": city, "count": 1}
+geo_res = requests.get(geo_url, params=geo_params).json()
+
+# API 1
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Weather API - rework maybe needed           
+if "results" in geo_res:
+    lat = geo_res["results"][0]["latitude"]
+    lon = geo_res["results"][0]["longitude"]
+
+    # Forecast subAPI
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": ["temperature_2m", "relative_humidity_2m", "wind_speed_180m", "wind_direction_180m", "uv_index"],
+        "current": ["temperature_2m", "relative_humidity_2m", "is_day", "wind_direction_10m", "wind_speed_10m",
+                    "apparent_temperature", "rain", "precipitation", "showers", "snowfall",
+                    "weather_code", "cloud_cover", "pressure_msl", "surface_pressure", "dust", "european_aqi"],
+        "minutely_15": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "wind_speed_80m", "wind_direction_80m", "is_day"],
+	    "timezone": "auto",
+	    "forecast_days": 1,
+	    "wind_speed_unit": "ms",
+        "models": "best_match"
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    response = responses[0]
+
+    minutely_15 = response.Minutely15()
+    minutely_15_temp = minutely_15.Variables(0).ValuesAsNumpy()
+    minutely_15_humidity = minutely_15.Variables(1).ValuesAsNumpy()
+    minutely_15_apptemp = minutely_15.Variables(2).ValuesAsNumpy()
+    minutely_15_windspeed = minutely_15.Variables(3).ValuesAsNumpy()
+    minutely_15_winddirection = minutely_15.Variables(4).ValuesAsNumpy()
+    minutely_15_is_day = minutely_15.Variables(5).ValuesAsNumpy()
+
+    minutely_15_data = {"date": pd.date_range(
+	    start = pd.to_datetime(minutely_15.Time(), unit = "s", utc = True),
+	    end =  pd.to_datetime(minutely_15.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = minutely_15.Interval()),
+	    inclusive = "left"
+    )}
+
+    minutely_15_data["temperature_2m"] = minutely_15_temp
+    minutely_15_data["relative_humidity_2m"] = minutely_15_humidity
+    minutely_15_data["apparent_temperature"] = minutely_15_apptemp
+    minutely_15_data["wind_speed_80m"] = minutely_15_windspeed
+    minutely_15_data["wind_direction_80m"] = minutely_15_winddirection
+    minutely_15_data["is_day"] = minutely_15_is_day
+
+    current = response.Current()
+    current_temp = current.Variables(0).Value()
+    current_humidity = current.Variables(1).Value()
+    current_is_day = current.Variables(2).Value()
+    current_winddirection = current.Variables(3).Value()
+    current_windspeed = current.Variables(4).Value()
+    current_apptemp = current.Variables(5).Value()
+    current_rain = current.Variables(6).Value()
+    current_precipitation = current.Variables(7).Value()
+    current_showers = current.Variables(8).Value()
+    current_snowfall = current.Variables(9).Value()
+    current_weather_code = current.Variables(10).Value()
+    current_cloud_cover = current.Variables(11).Value()
+    current_pressure_msl = current.Variables(12).Value()
+    current_surfpressure = current.Variables(13).Value()
+
+    response = responses[0]
+    hourly = response.Hourly()
+    hourly_temp = hourly.Variables(0).ValuesAsNumpy()
+    hourly_humidity = hourly.Variables(1).ValuesAsNumpy()
+    hourly_windspeed = hourly.Variables(2).ValuesAsNumpy()
+    hourly_winddirection = hourly.Variables(3).ValuesAsNumpy()
+    hourly_uvindex = hourly.Variables(4).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+
+    hourly_data["temperature_2m"] = hourly_temp
+    hourly_data["relative_humidity_2m"] = hourly_humidity
+    hourly_data["wind_speed_180m"] = hourly_windspeed
+    hourly_data["wind_direction_180m"] = hourly_winddirection
+    hourly_data["uv_index"] = hourly_uvindex
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+    minutely_15_dataframe = pd.DataFrame(data = minutely_15_data)
---
+++ weatherapp.py
@@ -0,0 +1,299 @@
+# Weather App
+# API data imports
+from weatherapi import city, hourly_data, current_temp, current_humidity, current_is_day, current_winddirection, current_windspeed, current_apptemp, current_precipitation, current_cloud_cover, current_pressure_msl, current_surfpressure
+from airqualityapi import current_dust, current_euroaqi, current_co, current_no2, current_o3, current_pm10, current_pm2_5, current_so2
+
+# Graphing imports
+import numpy as np
+import matplotlib as mpl
+from matplotlib.figure import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+# GUI imports
+import tkinter as tk
+import customtkinter as ctk
+from customtkinter import *
+import seaborn as sb
+import datetime as dt
+import ttkbootstrap as tkbs
+
+# GUI - rework needed
+class MasterFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+
+class SummaryFrame(ctk.CTkFrame):
+    def __init__(self, MasterFrame, **kwargs):
+        super().__init__(MasterFrame, **kwargs)
+        def airquality(self):
+            aqi_descriptor = "" 
+            if 0 >= current_euroaqi >= 20:
+                aqi_descriptor = "Healthy"
+            elif 21 >= current_euroaqi >= 40:
+                aqi_descriptor = "Fairly Healthy"
+            elif 41 >= current_euroaqi >= 60:
+                aqi_descriptor = "Moderate"
+            elif 61 >= current_euroaqi >= 80:
+                aqi_descriptor = "Unhealthy"
+            elif 81 >= current_euroaqi >= 100:
+                aqi_descriptor = "Very Unhealthy"
+            elif current_euroaqi >= 100:
+                aqi_descriptor = "Dangerous" 
+
+        airquality(self)
+
+        day_or_no = "🌞" if current_is_day == 1.0 else "🌙"
+        curtemp_text = f"{int(current_temp)}{'°C'}"
+        curhumidity_text = f"{int(current_humidity)}{' %'}"
+        curwinddir_text = f"{int(current_winddirection)}{' Deg'}"
+        curwindspeed_text = f"{int(current_windspeed)}{' m/s'}"
+        curprecip_text = f"{int(current_precipitation)}{' mm'}"
+        curpressuresl_text = f"{int(current_pressure_msl)}{' hPa'}"
+        curpressuresurf_text = f"{int(current_surfpressure)}{' hPa'}"
+        curapptemp_text = f"{'Feels like '}{int(current_apptemp)}{'°C'}"
+        curcloudcover_text = f"{int(current_cloud_cover)}{' %'}"
+        curdust_text = f"{int(current_dust)}{' μg/m^3'}"
+        curaqi_text = f"{int(current_euroaqi)}{' EU AQI'}"
+
+        self.curweather = ctk.CTkLabel(self, text = "Current weather")
+        self.current_temperature = ctk.CTkLabel(self, text = day_or_no + curtemp_text, font = ("Normal", 25, "normal"))
+        self.humidity_text = ctk.CTkLabel(self, text  = "Humidity")
+        self.current_humidity = ctk.CTkLabel(self, text = curhumidity_text)
+        self.winddirection_text = ctk.CTkLabel(self, text  = "Wind Direction")
+        self.current_winddirection = ctk.CTkLabel(self, text = curwinddir_text)
+        self.windspeed_text = ctk.CTkLabel(self, text  = "Wind Speed")
+        self.current_windspeed = ctk.CTkLabel(self, text = curwindspeed_text)
+        self.precip_text = ctk.CTkLabel(self, text = "Precipitation")
+        self.current_precip = ctk.CTkLabel(self, text = curprecip_text)
+        self.pressuresl_text = ctk.CTkLabel(self, text = "Sea Level Pressure")
+        self.current_pressuresl = ctk.CTkLabel(self, text = curpressuresl_text)
+        self.pressuresurf_text = ctk.CTkLabel(self, text = "Surface Pressure")
+        self.current_pressuresurf = ctk.CTkLabel(self, text = curpressuresurf_text)
+        self.current_apptemp = ctk.CTkLabel(self, text = curapptemp_text)
+        self.cloudcover_text = ctk.CTkLabel(self, text = "Cloud Cover")
+        self.current_cloudcover = ctk.CTkLabel(self, text = curcloudcover_text)
+        self.dust_text = ctk.CTkLabel(self, text = "Dustiness")
+        self.current_dust = ctk.CTkLabel(self, text = curdust_text)
+        self.aqi_text = ctk.CTkLabel(self, text = "Air Quality")
+        self.current_aqi = ctk.CTkLabel(self, text = curaqi_text)
+
+        self.curweather.grid(row = 0, column = 0, padx = 10, pady = 5)
+        self.current_temperature.grid(row = 1, column = 0, padx = 0, pady = 10)
+        self.humidity_text.grid(row = 2, column = 1, padx = 10, pady = 0, sticky = "s")
+        self.current_humidity.grid(row = 3, column = 1, padx = 10, pady = 2)  
+        self.winddirection_text.grid(row = 0, column = 2, padx = 10, pady = 0, sticky = "s")
+        self.current_winddirection.grid(row = 1, column = 2, padx = 10, pady = 2)
+        self.windspeed_text.grid(row = 2, column = 2, padx = 10, pady = 0, sticky = "s")
+        self.current_windspeed.grid(row = 3, column = 2, padx = 10, pady = 2)
+        self.precip_text.grid(row = 2, column = 4, padx = 10, pady = 0, sticky = "s")
+        self.current_precip.grid(row = 3, column = 4, padx = 10, pady = 2)
+        self.pressuresl_text.grid(row = 2, column = 5, padx = 10, pady = 0, sticky = "s")
+        self.current_pressuresl.grid(row = 3, column = 5, padx = 10, pady = 2)
+        self.pressuresurf_text.grid(row = 0, column = 5, padx = 10, pady = 0, sticky = "s")
+        self.current_pressuresurf.grid(row = 1, column = 5, padx = 10, pady = 2)
+        self.current_apptemp.grid(row = 3, column = 0, padx = 10, pady = 2)
+        self.cloudcover_text.grid(row = 0, column = 1, padx = 10, pady = 0, sticky = "s")
+        self.current_cloudcover.grid(row = 1, column = 1, padx = 10, pady = 2)
+        self.dust_text.grid(row = 0, column = 6, padx = 10, pady = 0, sticky = "s")
+        self.current_dust.grid(row = 1, column = 6, padx = 10, pady = 2)
+        self.aqi_text.grid(row = 2, column = 6, padx = 10, pady = 0, sticky = "s")
+        self.current_aqi.grid(row = 3, column = 6, padx = 10, pady = 2)
+
+class MoonFrame(ctk.CTkFrame):
+    def __init__(self, MasterFrame, **kwargs):
+        super().__init__(MasterFrame, **kwargs)
+    
+
+
+class GraphFrame(ctk.CTkFrame):
+    def __init__(self, MasterFrame, **kwargs):
+        super().__init__(MasterFrame, **kwargs)
+
+        def temp_weather_graph():
+            fig = Figure(figsize = (10, 5), facecolor ="#7A7A7A")
+
+            plot1 = fig.add_subplot(111)
+
+            plot1.plot(hourly_data["date"], hourly_data["temperature_2m"], label = "Temperature (°C)")
+
+            canvas = FigureCanvasTkAgg(fig, master = self)  
+            canvas.draw()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            toolbar = NavigationToolbar2Tk(canvas, self, pack_toolbar = False)
+            toolbar.update()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            plt.xlabel("By hours")
+            plt.ylabel("Temperature (°C)")
+            plt.title("Temperature 2m")  
+            
+        def wind_speed_weather_graph():
+            fig = Figure(figsize = (10, 5), facecolor ="#7A7A7A")
+
+            plot1 = fig.add_subplot(111)
+
+            plot1.plot(hourly_data["date"], hourly_data["wind_speed_180m"], label = "Wind Speed (m/s)")
+
+            canvas = FigureCanvasTkAgg(fig, master = self)  
+            canvas.draw()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            toolbar = NavigationToolbar2Tk(canvas, self, pack_toolbar = False)
+            toolbar.update()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            plt.xlabel("By hours")
+            plt.ylabel("Wind Speed (m/s)")
+            plt.title("Wind Speed 180m")
+
+        def wind_direction_weather_graph():
+
+            fig = Figure(figsize = (10, 5), facecolor ="#7A7A7A")
+
+            plot1 = fig.add_subplot(111)
+
+            plot1.plot(hourly_data["date"], hourly_data["wind_direction_180m"], label = "Direction (°)")
+
+            canvas = FigureCanvasTkAgg(fig, master = self)  
+            canvas.draw()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            toolbar = NavigationToolbar2Tk(canvas, self, pack_toolbar = False)
+            toolbar.update()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            plt.xlabel("By hours")
+            plt.ylabel("Wind Direction (°)")
+            plt.title("Wind Direction 180m")
+
+        def uv_weather_graph():
+            fig = Figure(figsize = (10, 5), facecolor ="#7A7A7A")
+
+            plot1 = fig.add_subplot(111)
+
+            plot1.plot(hourly_data["date"], hourly_data["uv_index"], label = "UV Index")
+
+            canvas = FigureCanvasTkAgg(fig, master = self)  
+            canvas.draw()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            toolbar = NavigationToolbar2Tk(canvas, self, pack_toolbar = False)
+            toolbar.update()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            plt.xlabel("By days")
+            plt.ylabel("UV Index")
+            plt.title("UV Index Today")
+
+        def humidity_weather_graph():
+            fig = Figure(figsize = (10, 5), facecolor ="#7A7A7A")
+
+            plot1 = fig.add_subplot(111)
+
+            plot1.plot(hourly_data["date"], hourly_data["relative_humidity_2m"], label ="Humidity (%)")
+
+            canvas = FigureCanvasTkAgg(fig, master = self)  
+            canvas.draw()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+
+            toolbar = NavigationToolbar2Tk(canvas, self, pack_toolbar = False)
+            toolbar.update()
+            canvas.get_tk_widget().grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "w")
+    
+            plt.xlabel("By hours")
+            plt.ylabel("Relative Humidity (%)")
+            plt.title("Relative Humidity 2m")  
+
+        self.temp_button = ctk.CTkButton(self, text = "Temperature", border_color = "#FFFFFF", command = temp_weather_graph)
+        self.windspeed_button = ctk.CTkButton(self, text = "Wind Speed", border_color = "#FFFFFF", command = wind_speed_weather_graph)
+        self.winddirection_button = ctk.CTkButton(self, text = "Wind Direction", border_color = "#FFFFFF", command = wind_direction_weather_graph)
+        self.uv_button = ctk.CTkButton(self, text = "UV Index", border_color = "#FFFFFF", command = uv_weather_graph)
+        self.humidity_button = ctk.CTkButton(self, text = "Humidity", border_color = "#FFFFFF", command = humidity_weather_graph)
+
+        self.temp_button.configure(height = 5, width = 15)
+        self.windspeed_button.configure(height = 5, width = 15)
+        self.winddirection_button.configure(height = 5, width = 15)
+        self.uv_button.configure(height = 5, width = 15) 
+        self.humidity_button.configure(height = 5, width = 15)
+
+        self.temp_button.grid(row = 0, column = 0, padx = 10, pady = 20, sticky = "nsew")
+        self.windspeed_button.grid(row = 0, column = 1, padx = 10, pady = 20, sticky = "nsew")
+        self.winddirection_button.grid(row = 0, column = 2, padx = 10, pady = 20, sticky = "nsew")
+        self.uv_button.grid(row = 0, column = 3, padx = 10, pady = 20, sticky = "nsew")
+        self.humidity_button.grid(row = 0, column = 4, padx = 10, pady = 20, sticky = "nsew")
+        
+class AirQualityFrame(ctk.CTkFrame):
+    def __init__(self, MasterFrame, **kwargs):
+        super().__init__(MasterFrame, **kwargs)
+
+        pm10_display = f"{int(current_pm10)}{' μg/m^3'}"
+        pm2_5_display = f"{int(current_pm2_5)}{' μg/m^3'}"
+        co_display = f"{int(current_co)}{' μg/m^3'}"
+        no2_display = f"{int(current_no2)}{' μg/m^3'}"
+        so2_display = f"{int(current_so2)}{' μg/m^3'}"
+        o3_display = f"{int(current_o3)}{' μg/m^3'}"
+
+        self.pm10_text = ctk.CTkLabel(self, text = "Particulate Matter 10")
+        self.current_pm10 = ctk.CTkLabel(self, text = pm10_display)
+        self.pm2_5_text = ctk.CTkLabel(self, text = "Particulate Matter 2.5")
+        self.current_pm25 = ctk.CTkLabel(self, text = pm2_5_display)
+        self.co_text = ctk.CTkLabel(self, text = "Carbon Dioxide")
+        self.current_co = ctk.CTkLabel(self, text = co_display)
+        self.no2_text = ctk.CTkLabel(self, text = "Nitrogen Dioxide") 
+        self.current_no2 = ctk.CTkLabel(self, text = no2_display)
+        self.so2_text = ctk.CTkLabel(self, text = "Sulfur Dioxide")
+        self.current_so2 = ctk.CTkLabel(self, text = so2_display)
+        self.o3_text = ctk.CTkLabel(self, text = "Ozone")
+        self.current_o3 = ctk.CTkLabel(self, text = o3_display)
+
+
+        self.pm10_text.grid(row = 0, column = 0, padx = 10, pady = 5)
+        self.current_pm10.grid(row = 1, column = 0, padx = 10, pady = 5)
+        self.pm2_5_text.grid(row = 0, column = 1, padx = 10, pady = 5)
+        self.current_pm25.grid(row = 1, column = 1, padx = 10, pady = 5)
+        self.co_text.grid(row = 0, column = 2, padx = 10, pady = 5 )
+        self.current_co.grid(row = 1, column = 2, padx = 10, pady = 5)
+        self.no2_text.grid(row = 0, column = 3, padx = 10, pady = 5)
+        self.current_no2.grid(row = 1, column = 3, padx = 10, pady = 5)
+        self.so2_text.grid(row = 0, column = 4, padx = 10, pady = 5)
+        self.current_so2.grid(row = 1, column = 4, padx = 10, pady = 5)
+        self.o3_text.grid(row = 0, column = 5, padx = 10, pady = 5)
+        self.current_o3.grid(row = 1, column = 5, padx = 10, pady = 5)
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Python Weather App")
+
+        self.grid_rowconfigure(0, weight = 1)
+        self.grid_columnconfigure(0, weight = 1)
+
+        self.master_frame = MasterFrame(master = self, height = 1200, width = 1200, corner_radius = 0, fg_color = "transparent")
+        self.master_frame.grid(row = 0, column = 0, sticky = "nsew")
+
+        self.city_label = ctk.CTkLabel(self.master_frame, text = city, font = ("Normal", 22, "bold"))
+        self.city_label.grid(row = 0, column = 0, padx = 10, pady = 20, sticky = "w")
+
+        self.display_frame = SummaryFrame(self.master_frame)
+        self.display_frame.grid(row = 1, column = 0, padx = 20, pady = 10, sticky = "w")
+
+        self.graph_text = ctk.CTkLabel(self.master_frame, text = "Hourly Weather", font = ("Normal", 20, "bold"))
+        self.graph_text.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = "sw")
+
+        self.button_frame = GraphFrame(self.master_frame)
+        self.button_frame.grid(row = 3, column = 0, padx = 20, pady = 20, sticky = "sw")
+
+        self.airqual_text = ctk.CTkLabel(self.master_frame, text = "Air Quality Details", font = ("Normal", 20, "bold"))
+        self.airqual_text.grid(row = 4, column = 0, padx = 10, pady = 10, sticky = "sw")
+
+        self.airqual_frame = AirQualityFrame(self.master_frame)
+        self.airqual_frame.grid(row = 5, column = 0, padx = 20, pady = 10, sticky = "sw")
+
+        
+app = App()
+app.mainloop()
